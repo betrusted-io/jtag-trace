23aae348b5e3b99aefadb0133c91e0590de34b74
bunnie <bunnie@kosagi.com>
2019-12-10 03:06:44 +0800

    fix bugs

--- jtag-trace.py
+++ jtag-trace.py
@@ -23,6 +23,122 @@ class JtagState(Enum):
 
 state = JtagState.RUN_TEST_IDLE
 
+def decode_ir(ir):
+    if ir == 0b100110:
+        return 'EXTEST'
+    elif ir == 0b111100:
+        return 'EXTEST_PULSE'
+    elif ir == 0b111101:
+        return 'EXTEST_TRAIN'
+    elif ir == 0b000001:
+        return 'SAMPLE'
+    elif ir == 0b000010:
+        return 'USER1'
+    elif ir == 0b000011:
+        return 'USER2'
+    elif ir == 0b100010:
+        return 'USER3'
+    elif ir == 0b100011:
+        return 'USER4'
+    elif ir == 0b000100:
+        return 'CFG_OUT'
+    elif ir == 0b000101:
+        return 'CFG_IN'
+    elif ir == 0b001001:
+        return 'IDCODE'
+    elif ir == 0b001010:
+        return 'HIGHZ_IO'
+    elif ir == 0b001011:
+        return 'JSTART'
+    elif ir == 0b001101:
+        return 'JSHUTDOWN'
+    elif ir == 0b110111:
+        return 'XADC_DRP'
+    elif ir == 0b010000:
+        return 'ISC_ENABLE'
+    elif ir == 0b010001:
+        return 'ISC_PROGRAM'
+    elif ir == 0b010010:
+        return 'XSC_PROGRAM_KEY'
+    elif ir == 0b010111:
+        return 'XSC_DNA'
+    elif ir == 0b110010:
+        return 'FUSE_DNA'
+    elif ir == 0b010100:
+        return 'ISC_NOOP'
+    elif ir == 0b010110:
+        return 'ISC_DISABLE'
+    elif ir == 0b111111:
+        return 'BYPASS'
+    elif ir == 0b110001:
+        return 'FUSE_KEY'
+    elif ir == 0b110011:
+        return 'FUSE_USER'
+    elif ir == 0b110100:
+        return 'FUSE_CNTL'
+    else:
+        return ''  # unknown just leave blank for now
+
+def debug_jtag(jtag_trace, index=0):
+    print('tms: ', end='')
+    for cycle in jtag_trace[index:]:
+        if cycle['tms']:
+            print('1', end='')
+        else:
+            print('0', end='')
+    print(' ')
+    print('tdi: ', end='')
+    for cycle in jtag_trace[index:]:
+        if cycle['tdi']:
+            print('1', end='')
+        else:
+            print('0', end='')
+    print(' ')
+
+
+def format_output(jtag_trace, cycle, departing_index, pause, loopbacks, leg, reg_in, reg_out):
+    #            if len(reg_in) < 6:
+    #debug_jtag(jtag_trace, departing_index)
+
+    if pause > 0 | loopbacks > 0:
+        print('Pauses: ', str(pause), '; loopbacks: ', str(loopbacks))
+    if leg == JtagLeg.DR:
+        print('Host->DR: ', end='')
+    else:
+        print('Host->IR: ', end='')
+
+    ir = 0
+    for bit in reversed(reg_in):  # this assumes data appears on JTAG TDI in LSB-first order
+        ir <<= 1
+        if bit:
+            print('1', end='')
+            ir |= 1
+        else:
+            print('0', end='')
+    if leg == JtagLeg.IR:
+        print(' (', decode_ir(ir), ')', end='')
+    else:
+        print(' (', hex(ir), ')', end='')
+    if cycle:
+        print(" ({:.3f}".format(cycle['time'] * 1000), 'ms)', end='')
+    print(' ')
+
+    if leg == JtagLeg.DR:
+        print('DR->Host: ', end='')
+    else:
+        print('IR->Host: ', end='')
+    reg = 0
+    for bit in reversed(reg_out):
+        reg <<= 1
+        if bit:
+            print('1', end='')
+            reg |= 1
+        else:
+            print('0', end='')
+    if leg == JtagLeg.DR:
+        print(' (', hex(reg), ')', end='')
+    print(' ')
+
 # take a trace and attempt to extract IR, DR values
 # assume: at the start of each 'trace' we are coming from TEST-LOGIC-RESET
 def jtag_mach(jtag_trace):
@@ -33,8 +149,11 @@ def jtag_mach(jtag_trace):
     leg = JtagLeg.DR
     pause = 0
     loopbacks = 0
+    index = -1
+    departing_index = 0
 
     for cycle in jtag_trace:
+        index = index + 1
         if state == JtagState.TEST_LOGIC_RESET:
             if cycle['tms']:
                 continue
@@ -48,6 +167,7 @@ def jtag_mach(jtag_trace):
             pause = 0
             loopbacks = 0
             leg = JtagLeg.DR
+            departing_index = index
             if cycle['tms']:
                 state = JtagState.SELECT_DR_SCAN
                 continue
@@ -61,6 +181,7 @@ def jtag_mach(jtag_trace):
                 continue
             else:
                 state = JtagState.CAPTURE
+                continue
 
         elif state == JtagState.SELECT_IR_SCAN:
             leg = JtagLeg.IR
@@ -69,6 +190,7 @@ def jtag_mach(jtag_trace):
                 continue
             else:
                 state = JtagState.CAPTURE
+                continue
 
         elif state == JtagState.CAPTURE:
             if cycle['tms']:
@@ -108,39 +230,31 @@ def jtag_mach(jtag_trace):
             continue
 
         elif state == JtagState.UPDATE:
-            if pause > 0 | loopbacks > 0:
-                print('Pauses: ', str(pause), '; loopbacks: ', str(loopbacks))
-            if leg == JtagLeg.DR:
-                print( 'Host -> DR: ', end='')
-            else:
-                print( 'Host -> IR: ', end='')
-            for bit in reversed(reg_in):  # this assumes data appears on JTAG TDI in LSB-first order
-                if bit:
-                    print('1', end='')
-                else:
-                    print('0', end='')
-            print(' ')
-
-            if leg == JtagLeg.DR:
-                print( 'DR -> Host: ', end='')
-            else:
-                print( 'IR -> Host: ', end='')
-            for bit in reversed(reg_out):
-                if bit:
-                    print('1', end='')
-                else:
-                    print('0', end='')
-            print(' ')
-
+            format_output(jtag_trace, cycle, departing_index, pause, loopbacks, leg, reg_in, reg_out)
             if cycle['tms']:
                 state = JtagState.SELECT_DR_SCAN
+                # recycle all parameters as we are entering a leg select again
+                reg_in = []
+                reg_out = []
+                pause = 0
+                loopbacks = 0
+                leg = JtagLeg.DR
+                departing_index = index
+                continue
             else:
                 state = JtagState.RUN_TEST_IDLE
-            continue
+                continue
 
         else:
             print("Illegal state encountered!")
             continue
+
+    if state == JtagState.UPDATE:
+        format_output(jtag_trace, False, departing_index, pause, loopbacks, leg, reg_in, reg_out)
+        # From a TMS standpoint, it's isomorphic to move to RUN_IDLE; but if we leave it in UPDATE then the
+        # next call into the loop will print all the output again, which is incorrect
+        state = JtagState.RUN_TEST_IDLE
+
 
 def main():
     parser = argparse.ArgumentParser(description="Parse JTAG waveform")
@@ -192,11 +306,11 @@ def main():
             else:
                 tdo = False
 
-            jtag_trace += [{'tdi': tdi, 'tms': tms, 'tdo': tdo}]
+            jtag_trace += [{'tdi': tdi, 'tms': tms, 'tdo': tdo, 'time': float(row[0])}]
             last_time = float(row[0])
 
     for packet in jtag_packets:
-        print('\n**** Start packet at ', "{:.3f}".format(packet['start'] * 1000), 'ms')
+        # print('\n**** Start packet at ', "{:.3f}".format(packet['start'] * 1000), 'ms')
         jtag_mach(packet['trace'])
 
 
